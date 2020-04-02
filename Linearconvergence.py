e3cf253c763da067a2656c23d9fc070127b62d03
Jafaranafi <56313953+Jafaranafi@users.noreply.github.com>
2020-04-02 19:56:04 +0100

    initial commit

---
+++ Linearconvergence.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+def f(x):
+	return x**3+x-1
+def g(x):
+	return 2*x**2+1
+n=18
+#x=2
+u=np.zeros(19)
+u[0]=1
+
+for i in range(n):
+	u[i+1]=u[i]-f(u[i])/g(u[i])
+	c=(abs(u[i+1]-u[0]))/(abs(u[i]-u[0]))
+	print (c)
+
